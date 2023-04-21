739ef0f56fc5f6ee7b1c9b8e7e9d110c152a7c82
Anastasiia <[afina865@gmail.com]>
2023-04-21 09:20:27 +0300

    task_15

---
+++ PHP/tasks20/task_15_login.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$email = $_POST['email'];
+$password = $_POST['password'];
+$pdo = new PDO('mysql:host=localhost;dbname=tasks_15', 'root', '');
+$sql = "SELECT * FROM `users` WHERE email=:email and password=:password";
+$statement = $pdo->prepare($sql);
+$statement->execute(['email' => $email, 'password'=>$password]);
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+if($result==false){
+    $message = 'Неверный логин или пароль';
+    $_SESSION['mistake']=$message;
+}
+else{
+    $message = 'Все правильно!';
+    $_SESSION['right']=$message;
+}
+header("Location:/PHP/tasks20/task_15.php");
