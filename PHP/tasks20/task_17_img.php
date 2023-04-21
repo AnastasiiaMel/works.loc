4fed7d0d7714bc5cad52f4af7f206d657654f979
Anastasiia <[afina865@gmail.com]>
2023-04-21 15:03:23 +0300

    task_17

---
+++ PHP/tasks20/task_17_img.php
@@ -0,0 +1,22 @@
+<?php
+
+$img_name=$_FILES['img']['name'];
+$img_tmp_name=$_FILES['img']['tmp_name'];
+$tmp_upload = 'upload_task17/';
+$uniq_name = uniqid();
+$extension = explode('.', $img_name);
+$new_name = $uniq_name.'.'.$extension[array_key_last($extension)];
+$new_tmp =$tmp_upload.$new_name;
+move_uploaded_file($img_tmp_name, $new_tmp);
+
+
+$pdo = new PDO('mysql:host=localhost;dbname=task_17_img', 'root', '');
+$sql = "INSERT INTO `img` (`id`, `name`, `path`) VALUES (NULL, :name, :path);";
+$statement = $pdo->prepare($sql);
+$statement->execute(['name'=> $img_name, 'path'=> $new_name]);
+
+
+
+
+
+header("Location:/PHP/tasks20/task_17.php");
