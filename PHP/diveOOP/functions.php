f43b1644e201ce032859b2da40261bf98b004c8c
Anastasiia <[afina865@gmail.com]>
2023-04-25 10:42:31 +0300

    register video(1, 2)

---
+++ PHP/diveOOP/functions.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+function get_user_by_email($email){
+    $pdo = new PDO('mysql:host=localhost;dbname=my_project', 'root', '');
+    $sql = "SELECT * FROM users WHERE email=:email";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['email' => $email]);
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+    return $user;
+}
+
+function set_flash_message($name, $massage){
+    $_SESSION[$name] = $massage;
+}
+
+function redirect_to($path){
+    header("Location: {$path}");
+    exit;
+}
+
+function add_user($email, $password){
+    $pdo = new PDO('mysql:host=localhost;dbname=my_project', 'root', '');
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $statement = $pdo->prepare($sql);
+    $result = $statement->execute([
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_DEFAULT)
+    ]);
+
+    return $pdo->lastInsertId();
+}
+
+function display_flash_message($name){
+    if (isset($_SESSION[$name])){
+        echo "<div class=\"alert alert-{$name} text-dark\" role=\"alert\">{$_SESSION[$name]}</div>";
+        unset($_SESSION[$name]);
+    }
+}
