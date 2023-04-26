5ce653225e01eb4ff1e11692ea76c703931cda2c
Anastasiia <[afina865@gmail.com]>
2023-04-26 08:39:27 +0300

    add description function (1.3)

--- PHP/diveOOP/functions.php
+++ PHP/diveOOP/functions.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+/**
+ *  Parameters:
+ *          string - $email
+ *  Description: поиск пользователя по эл. адресу
+ *  Return value: array
+ */
+
 function get_user_by_email($email){
     $pdo = new PDO('mysql:host=localhost;dbname=my_project', 'root', '');
     $sql = "SELECT * FROM users WHERE email=:email";
@@ -9,14 +17,37 @@ function get_user_by_email($email){
     return $user;
 }
 
+/**
+ *  Parameters:
+ *          string - $name (ключ)
+ *          string - $message (значение, текст сообщения)
+ *  Description: подготовить флеш сообщение
+ *  Return value: null
+ */
+
 function set_flash_message($name, $massage){
     $_SESSION[$name] = $massage;
 }
+
+/**
+ *  Parameters:
+ *          string - $path
+ *  Description: перенаправить на другую страницу
+ *  Return value: null
+ */
 
 function redirect_to($path){
     header("Location: {$path}");
     exit;
 }
+
+/**
+ *  Parameters:
+ *          string - $email
+ *          string - $password
+ *  Description: добавить пользователя в БД
+ *  Return value: int (user_id)
+ */
 
 function add_user($email, $password){
     $pdo = new PDO('mysql:host=localhost;dbname=my_project', 'root', '');
@@ -29,6 +60,13 @@ function add_user($email, $password){
 
     return $pdo->lastInsertId();
 }
+
+/**
+ *  Parameters:
+ *          string - $name (ключ)
+ *  Description: выыести флеш сообщение
+ *  Return value: null
+ */
 
 function display_flash_message($name){
     if (isset($_SESSION[$name])){
