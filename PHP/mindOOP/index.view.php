a283ad5ed03c20165cc7880e80adeeaffd41c7aa
Anastasiia <[afina865@gmail.com]>
2023-05-04 16:36:10 +0300

    add new file and refactoring code (3 video)

---
+++ PHP/mindOOP/index.view.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html land="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+          crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">My blog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">MainPage</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<div class="conteiner">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <a href="#" class="btn btn-success" >Add post</a>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($posts as $post): ?>
+                    <tr>
+                        <th scope="row"><?php echo $post['id'];?></th>
+                        <td><?php echo $post['title'];?></td>
+                        <td>
+                            <a href="#" class="btn btn-warning">Edit</a>
+                            <a href="#" class="btn btn-danger" >Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
