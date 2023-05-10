cbd2b2d70eb0c5433c00ab8ac829ece1f94d51c4
Anastasiia <[afina865@gmail.com]>
2023-05-10 18:05:09 +0300

    add method update and first in class DB (8 video)

--- PHP/developmentPureOOP/index.php
+++ PHP/developmentPureOOP/index.php
@@ -3,10 +3,19 @@ require_once 'Database.php';
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?,?)", ['John Doe', 'Jane Koe']);
 //$users=Database::getInstance()->get('users', ['password', '=', 'password']);
 //Database::getInstance()->delete('users', ['username', '=', 'Jane Koe']);
-$users=Database::getInstance()->insert('users', [
-    'username' => 'Marlin',
-    'password' => 'password'
-]);
+//$users=Database::getInstance()->insert('users', [
+   // 'username' => 'Marlin',
+    //'password' => 'password'
+//]);
+
+//$id=3;
+//$users=Database::getInstance()->update('users',$id, [
+  //  'username' => 'Marlin2',
+    //'password' => 'password2'
+//]);
+
+$users=Database::getInstance()->get('users', ['username', '=', 'Marlin2']);
+echo $users->first()->username;
 
 //if($users->error()){
   //  echo 'We have an error';
