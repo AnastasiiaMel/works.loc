17cd47f53f8bf465f2fe518c386df903264d9277
Anastasiia <[afina865@gmail.com]>
2023-05-10 16:50:10 +0300

    add method get and delete in class DB (6 video)

--- PHP/developmentPureOOP/index.php
+++ PHP/developmentPureOOP/index.php
@@ -1,19 +1,17 @@
 <?php
 require_once 'Database.php';
-$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?,?)", ['John Doe', 'Jane Koe']);
-//Database::getInstance()->get('users', ['username', '=', 'marlin']);
-//Database::getInstance()->delete('users', ['username', '=', 'marlin']);
+//$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?,?)", ['John Doe', 'Jane Koe']);
+$users=Database::getInstance()->get('users', ['password', '=', 'password']);
+//Database::getInstance()->delete('users', ['username', '=', 'Jane Koe']);
 
 
 if($users->error()){
     echo 'We have an error';
-}
-else{
+} else{
     foreach ($users->results() as $user){
         echo $user->username . '<br>';
     }
 }
 
-if ($users->count()){
 
-}
+
