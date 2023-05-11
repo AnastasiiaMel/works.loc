ac002a548e818d838e70d5e26d904fc7a6ba54ef
Anastasiia <[afina865@gmail.com]>
2023-05-11 22:45:54 +0300

    add class Input and Validate (10 video)

--- PHP/developmentPureOOP/index.php
+++ PHP/developmentPureOOP/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'Database.php';
 require_once 'Config.php';
+require_once 'Validate.php';
+require_once 'Input.php';
+
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?,?)", ['John Doe', 'Jane Koe']);
 //$users=Database::getInstance()->get('users', ['password', '=', 'password']);
 //Database::getInstance()->delete('users', ['username', '=', 'Jane Koe']);
@@ -39,7 +42,57 @@ $GLOBALS['config'] = [
 ];
 
 //echo Config::get('mysql.host');
-$users = Database::getInstance()->query('select * from users');
-var_dump($users->results());
+//$users = Database::getInstance()->query('select * from users');
+//var_dump($users->results());
 
 
+if (Input::exists()){
+    $validate = new Validate();
+
+    $validation = $validate->check($_POST, [
+        'username' => [
+            'required' => true,
+            'min' => 2,
+            'max'=>15,
+            'unique' => 'users'
+        ],
+        'password' => [
+            'required' => true,
+            'min' => 3
+        ],
+        'password_again' => [
+            'required' => true,
+            'matches' => 'password'
+        ]
+    ]);
+
+    if ($validation->passed()){
+        echo 'passed';
+    }else{
+        foreach ($validation->errors() as $error){
+            echo $error . '<br>';
+        }
+    }
+}
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" value="<?php echo Input::get('username') ?>">
+    </div>
+
+    <div class="field">
+        <label for="">Password</label>
+        <input type="text" name="password">
+    </div>
+
+    <div class="field">
+        <label for="">Password Again</label>
+        <input type="text" name="password_again">
+    </div>
+
+    <div class="field">
+        <button type="submit">Submit</button>
+    </div>
+</form>
