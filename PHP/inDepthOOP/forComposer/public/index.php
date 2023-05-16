6e7d504e7235c87799d9bc7876a320e6c70c2825
Anastasiia <[afina865@gmail.com]>
2023-05-16 13:01:45 +0300

    Routing 1  (11 video, video 10 not found in curse)

--- PHP/inDepthOOP/forComposer/public/index.php
+++ PHP/inDepthOOP/forComposer/public/index.php
@@ -2,10 +2,59 @@
 
 require "../../vendor/autoload.php";
 
-use App\QueryBuilder;
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/PHP/inDepthOOP/forComposer/users', 'get_all_users_handler');
+    // {id} must be a number (\d+)
+    $r->addRoute('GET', '/PHP/inDepthOOP/forComposer/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+    $r->addRoute('GET', '/PHP/inDepthOOP/forComposer/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
 
-$db = new QueryBuilder();
-d($db);
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
+
+function get_user_handler($vars){
+    d($vars['id']);
+}
+
+
+
+
+
+
+
+
+
+//use App\QueryBuilder;
+
+//$db = new QueryBuilder();
+//d($db);
 
 
 
